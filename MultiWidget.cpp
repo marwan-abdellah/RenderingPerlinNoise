87648094838902aa9b528b432b8f349de5ae1873
marwan-abdellah <abdellah.marwan@gmail.com>
2013-05-18 16:57:02 +0200

    Adjusting the window creation parameters.

--- MultiWidget.cpp
+++ MultiWidget.cpp
@@ -6,12 +6,18 @@
 using namespace std;
 
 
-#define GAP  25             /* gap between subwindows */
-GLuint window, View1, View2, View3, View4;
+// Gap between the different viewports
+#define GAP  10
+
 GLuint sub_width = 256, sub_height = 256;
 
-void main_display(void);
-void main_reshape(int width,  int height);
+GLuint window;
+GLuint view1, view2, view3, view4;
+GLuint screenWidth;
+GLuint screenHeight;
+
+void mainDisplay(void);
+void mainReshape(int width,  int height);
 void setfont(char* name, int size);
 void drawstr(GLuint x, GLuint y, const char* format, int length);
 GLvoid *font_style = GLUT_BITMAP_TIMES_ROMAN_10;
@@ -23,81 +29,114 @@ void View4Display();
 
 void ResetViewport();
 
+///
 int main(int argc, char** argv)
 {
+    // GLUT initialization
     glutInit(&argc, argv);
 
-    //Mode Setting
+    // getting the screen resolution to adjust the rest of the application accordinglly
+    screenWidth = glutGet(GLUT_SCREEN_WIDTH);
+    screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
+
+    cout << "SCREEN_WIDTH : " << screenWidth << endl;
+    cout << "SCREEN_HEIGHT : " << screenHeight << endl;
+
+    // GLUT mode settings, RGB, depth buffer, double buffering
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    //window size (+gap size)
-    glutInitWindowSize(512+GAP*3, 512+GAP*3);
-    //Initial position
-    glutInitWindowPosition(50, 50);
 
+    // Center the window in the middle of the screen
+    int windowPosX = (screenWidth / 2) - (screenWidth / 4);
+    int windowPosY = (screenHeight / 2) - (screenHeight / 4);
 
+    // Initial window position
+    glutInitWindowPosition(windowPosX, windowPosY);
 
-    //Main Window
-    window = glutCreateWindow("ViewPort Test");
-    //Main Window callback function
-    glutReshapeFunc(main_reshape);
-    glutDisplayFunc(main_display);
+    // Make square widgets
+    if (screenWidth != screenHeight)
+    {
+        if (screenWidth < screenHeight)
+            screenHeight = screenWidth;
+        else
+            screenWidth = screenHeight;
+    }
 
-    //World Window and Display
-    View1 = glutCreateSubWindow(window, GAP, GAP, 256, 256);
+    // Window size inclusing the gaps
+    glutInitWindowSize(screenWidth, screenHeight);
+
+    // Main window creation
+    window = glutCreateWindow("MultiViewPort Window (multi Widgets)");
+
+    // Main window callback function
+    glutReshapeFunc(mainReshape);
+    glutDisplayFunc(mainDisplay);
+
+    // Calculating the viewports dimensions
+    float viewportWidth = screenWidth / 4;
+    float viewportHeight = screenHeight / 4;
+
+    // View 1 viewport
+    view1 = glutCreateSubWindow(window, GAP, GAP,
+                                viewportWidth, viewportHeight);
     glutDisplayFunc(View1Display);
 
-    //screen Window and Display
-    View2 = glutCreateSubWindow(window, GAP+256+GAP, GAP, 256, 256);
+    // View 2 viewport
+    view2 = glutCreateSubWindow(window, GAP + viewportWidth + GAP,
+                                GAP, viewportWidth, viewportHeight);
     glutDisplayFunc(View2Display);
 
-    //command Window and Display
-    View3 = glutCreateSubWindow(window, GAP+256+GAP, GAP+256+GAP, 256, 256);
+    // View 3 viewport
+    view3 = glutCreateSubWindow(window, GAP+viewportWidth+GAP,
+                                GAP + viewportHeight + GAP, viewportWidth, viewportHeight);
     glutDisplayFunc(View3Display);
 
-    View4 = glutCreateSubWindow(window, GAP+256+GAP, GAP+256+GAP, 256, 256);
+    // View 4 viewport
+    view4 = glutCreateSubWindow(window, GAP + viewportWidth + GAP,
+                                GAP + viewportHeight + GAP, viewportWidth, viewportHeight);
     glutDisplayFunc(View4Display);
 
-
+    /// MAIN LOOP ! Should be multi-threaded
     glutMainLoop();
 
     return 0;
 }
 
-void main_display(void)
+///
+void mainDisplay(void)
 {
-    //Background Color
+    // Background Color
     glClearColor(0.8, 0.8, 0.8, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //font color and style
+    // Font color and style
     glColor3ub(0, 0, 0);
     setfont("helvetica", 12);
 
-    //1st window name
-    string str = "View1";
+    // Window 1 name
+    string str = "Window 1";
     drawstr(GAP, GAP-5, str.c_str(), str.length());
 
-    //2st window name
-    str = "View2";
-    drawstr(GAP+sub_width+GAP, GAP-5, str.c_str(), str.length());
+    // Window 2 name
+    str = "Window 2";
+    drawstr(GAP + sub_width+GAP, GAP - 5, str.c_str(), str.length());
 
-    //3st widnow name
-    str = "View3";
-    drawstr(GAP, GAP+sub_height+GAP-5, str.c_str(), str.length());
+    // Window 3 name
+    str = "Window 3";
+    drawstr(GAP, GAP + sub_height + GAP - 5, str.c_str(), str.length());
 
-    //4st widnow name
-    str = "View4";
-    drawstr(GAP+sub_width+GAP, GAP+sub_height+GAP-5, str.c_str(), str.length());
+    // Window 4 name
+    str = "Window 4";
+    drawstr(GAP + sub_width + GAP, GAP + sub_height + GAP - 5, str.c_str(), str.length());
 
-    //last
+    /// BUFFER SWAPPING ! Double buffering
     glutSwapBuffers();
 }
 
 
-//Background Window Setting
-void main_reshape(int width,  int height)
+///
+void mainReshape(int width,  int height)
 {
-    //main view setting
+    // Main view setting
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -108,24 +147,24 @@ void main_reshape(int width,  int height)
     sub_width = (width-GAP*3)/2.0;
     sub_height = (height-GAP*3)/2.0;
 
-    //View1 Display
-    glutSetWindow(View1);
+    //view1 Display
+    glutSetWindow(view1);
     glutPositionWindow(GAP, GAP);
     glutReshapeWindow(sub_width, sub_height);
 
-    //View2 Display
-    glutSetWindow(View2);
+    //view2 Display
+    glutSetWindow(view2);
     glutPositionWindow(GAP+sub_width+GAP, GAP);
     glutReshapeWindow(sub_width, sub_height);
 
-    //View3 Display
-    glutSetWindow(View3);
+    //view3 Display
+    glutSetWindow(view3);
     glutPositionWindow(GAP, GAP+sub_height+GAP);
     glutReshapeWindow(sub_width, sub_height);
 
 
-    //View4 Display
-    glutSetWindow(View4);
+    //view4 Display
+    glutSetWindow(view4);
     glutPositionWindow(GAP+sub_width+GAP, GAP+sub_height+GAP);
     glutReshapeWindow(sub_width, sub_height);
     //glutReshapeWindow(sub_width+GAP+sub_width, sub_height);
