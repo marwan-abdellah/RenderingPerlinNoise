e4888b3604bb3213270d7b8ed51d2bfa5eb160b7
marwan-abdellah <abdellah.marwan@gmail.com>
2013-05-19 15:33:35 +0200

    Adding the license.

--- ReadVolume.hpp
+++ ReadVolume.hpp
@@ -6,6 +6,25 @@
 #include <stdio.h>
 #include <fstream>
 
+/*********************************************************************
+ * Copyright © 2011-2012,
+ * Marwan Abdellah: <abdellah.marwan@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ ********************************************************************/
+
 #include "Shared.h"
 #include "GL_Utils.h"
 #include "Utils.h"
